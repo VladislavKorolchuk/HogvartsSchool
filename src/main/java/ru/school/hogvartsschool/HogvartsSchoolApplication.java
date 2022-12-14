b4a52ed060257daa4def0fdbe23dfac1de010b86
93294 <9329496@mail.ru>
2022-12-14 17:53:33 +0300

    Initial commit

---
+++ src/main/java/ru/school/hogvartsschool/HogvartsSchoolApplication.java
@@ -0,0 +1,13 @@
+package ru.school.hogvartsschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogvartsSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogvartsSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/school/hogvartsschool/HogvartsSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.school.hogvartsschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogvartsSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
